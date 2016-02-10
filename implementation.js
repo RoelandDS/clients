1deaae555f6ff705d4c16a2b3945d7115df019cb
feveraer <frederic.everaert@gmail.com>
2016-02-10 11:22:45 +0100

    client implementation

--- implementation.js
+++ implementation.js
@@ -2,7 +2,32 @@
 var request = require('request');
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/workshop');
+
+// Schema
+var clientSchema = new mongoose.Schema({
+	"﻿Gender" : String,
+	"GivenName" : String,
+	"MiddleInitial" : String,
+	"Surname" : String,
+	"StreetAddress" : String,
+	"City" : String,
+	"State" : String,
+	"StateFull" : String,
+	"ZipCode" : String,
+	"Country" : String,
+	"CountryFull" : String,
+	"Birthday" : Date,
+	"Occupation": String
+});
+
+var Client = mongoose.model('Client', clientSchema);
+
 exports.getClients = function(args, done){
-	console.log('hello');
-	return done(null, {message: 'up and running'});
+
+	Client.find().limit(10).lean().exec(function (err, clients) {
+		return done(null, { clients: JSON.stringify(clients) });
+	});
+
 };
+
